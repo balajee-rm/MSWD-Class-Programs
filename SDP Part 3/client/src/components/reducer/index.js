e0dee5ede4a88d908c37be3d2b454a263675e690
balajee-rm <69756772+balajee-rm@users.noreply.github.com>
2023-02-11 08:54:07 +0530

    Delete SDP Part 3 directory

--- SDP Part 3/client/src/components/reducer/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import AuthenticationReducer from "./AuthenticationReducer"
-import NavigationReducer from "./NavigationReducer";
-
-export default combineReducers({
-    AuthenticationReducer: AuthenticationReducer,
-    NavigationReducer: NavigationReducer
-});
